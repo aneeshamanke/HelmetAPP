27aa31b401b7d46b9d954d0ca4d73c378365849a
TanishkaDeshpande <tanishkasvdeshpande@gmail.com>
2022-06-16 14:29:03 +0530

    first commit

---
+++ app/src/main/java/com/example/helmetapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.helmetapp
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import java.util.regex.Pattern
+
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun openWebURL(view: View) {
+        val browse = Intent(Intent.ACTION_VIEW, Uri.parse("https://meet.google.com/kux-sqbh-bwy"))
+        startActivity(browse)
+    }
+
+}
